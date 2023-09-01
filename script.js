df46571ccb59c7c1606437cea350bd02cf1d84ba
Fernando <lfernando.div@gmail.com>
2023-08-31 23:44:26 -0300

    Fit/writing texts for images when it's changing

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  // pegar a tag img
+  // get tag img
   const img = document.querySelector("#profile img")
+
+  // replace the image
+  if (html.classList.contains("light")) {
+    // if it's in light mode, add the light image
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos de sol e jaqueta preta, sem barba e fundo azul gradiente."
+    )
+  } else {
+    // if it's not in light mode, keep the normal image
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+    )
+  }
 }
